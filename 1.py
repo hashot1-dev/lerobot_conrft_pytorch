0ca39c59f5b1d77e2f2a76ecb3f5b0cfb9e38519
李力 <lili@s-robot.com>
2025-10-16 18:57:52 +0800

    Initial commit

---
+++ 1.py
@@ -0,0 +1,8 @@
+
+import torch
+print("PyTorch version:", torch.__version__)
+print("CUDA available:", torch.cuda.is_available())
+if torch.cuda.is_available():
+    print("GPU name:", torch.cuda.get_device_name(0))
+    x = torch.rand(10000, 10000, device="cuda")
+    print("Tensor sum:", x.sum().item())
